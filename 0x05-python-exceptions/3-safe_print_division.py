630b9aae259f92f1f1c6721a6ae64c9a8689d818
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-11 16:34:35 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-        print("Inside result: {:.1f}".format(result))
-    except:
-        result = None
-        print("Inside result: {}".format(result))
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
     finally:
-        return result
+        print("Inside result: {}".format(div))
+    return div
+Footer
+© 2022 GitHub, Inc.
