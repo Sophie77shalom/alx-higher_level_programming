76a6b77276ffc9c4ab666616eeacd54ad32d0843
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-18 18:28:48 +0300

    Update 101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class LockedClass with no class or object attribute.
+"""
+
+
+class LockedClass:
+    """Class with object LockedClass.
+    """
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Method: initialize.
+        """
+        pass
