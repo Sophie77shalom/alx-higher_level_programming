521cc61f72ba6d3862e67a17ec9c5875c6281aa9
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-19 07:20:41 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,59 +1,30 @@
 #!/usr/bin/python3
-
 """Unittest for max_integer function.
-
 """
-
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 
-
-
-
 class TestMaxInteger(unittest.TestCase):
-
     """Tests: functions max_integer
-
     """
 
-
-
     def test_empty_list(self):
-
         self.assertEqual(max_integer([]), None)
 
-
-
     def test_max_integer(self):
-
         self.assertEqual(max_integer([6, 7, 8, 9]), 9)
 
-
-
     def test_max_integer(self):
-
         self.assertEqual(max_integer([-6, -7, -8, -9]), -6)
 
-
-
     def test_zero_number(self):
-
         self.assertEqual(max_integer([0, 0]), 0)
 
-
-
     def test_string_number_in_a_list(self):
-
         with self.assertRaises(TypeError):
-
             max_integer([0, '1'])
 
-
-
     def test_tuple_in_a_list(self):
-
         with self.assertRaises(TypeError):
-
             max_integer([10, (20, 30)])
