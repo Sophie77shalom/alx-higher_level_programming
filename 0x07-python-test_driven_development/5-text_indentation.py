6c79e8c5426114ebf7a6163ab3eaa4bf4c3d9974
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-19 07:05:22 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,50 +1,26 @@
 #!/usr/bin/python3
 """Function that prints a test with new lines.
-
 """
 
 
-
-
-
 def text_indentation(text):
-
     """Method: prints a text with 2 new lines after given characters.
-
     Args:
-
     text(str): text
-
     Returns:
-
     No return
-
     """
 
-
-
     if not isinstance(text, str):
-
         raise TypeError("text must be a string")
-
-
 
     s = text[:]
 
-
-
     for d in ".?:":
-
         list_text = s.split(d)
-
         s = ""
-
         for i in list_text:
-
             i = i.strip(" ")
-
             s = i + d if s is "" else s + "\n\n" + i + d
-
-
 
     print(s[:-3], end="")
