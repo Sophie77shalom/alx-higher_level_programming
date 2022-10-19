eb0fe37c3304df65beb109d7985788d48781af59
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-19 07:02:47 +0300

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,36 +1,19 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """Function that prints first name and last name.
-
 """
 
 
-
-
-
 def say_my_name(first_name, last_name=""):
-
     """Method: function that prints first name and last name.
-
     Args:
-
     first_name(str): the first name
-
     last_name(str): the last name
-
     Returns:
-
     No return
-
     """
 
-
-
     if not isinstance(first_name, str):
-
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
-
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))
