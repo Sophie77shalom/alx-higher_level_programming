b11c392fe972db987383e3f13ad8f682e0c0eba5
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-19 07:23:30 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,30 +1,71 @@
 #!/usr/bin/python3
-"""Unittest for max_integer function.
+"""Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Tests: functions max_integer
-    """
+    """unittest class for max_integer"""
+    def test_module_docstring(self):
+        """Tests for module docsting"""
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
+
+    def test_function_docstring(self):
+        """Tests for funstion docstring"""
+        f = max_integer.__doc__
+        self.assertTrue(len(f) > 1)
 
     def test_empty_list(self):
-        self.assertEqual(max_integer([]), None)
+        """Tests for empty list []"""
+        e = []
+        self.assertIsNone(max_integer(e))
 
-    def test_max_integer(self):
-        self.assertEqual(max_integer([6, 7, 8, 9]), 9)
+    def test_no_args(self):
+        """Tests for no arguments passed to func"""
+        self.assertIsNone(max_integer())
 
-    def test_max_integer(self):
-        self.assertEqual(max_integer([-6, -7, -8, -9]), -6)
+    def test_one_element(self):
+        """Tests for only one number in the list"""
+        o = [1]
+        self.assertEqual(max_integer(o), 1)
 
-    def test_zero_number(self):
-        self.assertEqual(max_integer([0, 0]), 0)
+    def test_positive_end(self):
+        """Tests for all positive with max at end"""
+        e = [2, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(e), 50)
 
-    def test_string_number_in_a_list(self):
+    def test_positive_middle(self):
+        """Tests for all positive with max in middle"""
+        m = [2, 10, 8, 360, 14, 50]
+        self.assertEqual(max_integer(m), 360)
+
+    def test_positive_beginning(self):
+        """Tests for all positive with max at beginning"""
+        b = [200, 10, 8, 36, 14, 50]
+        self.assertEqual(max_integer(b), 200)
+
+    def test_one_negative(self):
+        """Tests for list with one negative number"""
+        on = [200, 10, 8, -36, 14, 50]
+        self.assertEqual(max_integer(on), 200)
+
+    def test_all_negative(self):
+        """Tests for list with all negative numbers"""
+        n = [-6, -50, -75, -1, -100]
+        self.assertEqual(max_integer(n), -1)
+
+    def test_none(self):
+        """Tests for passing none as argument"""
         with self.assertRaises(TypeError):
-            max_integer([0, '1'])
+            max_integer(None)
 
-    def test_tuple_in_a_list(self):
+    def test_non_int_arg(self):
+        """Tests for a non-int type in list"""
+        string = [1, 2, "Hello", 4, 5]
         with self.assertRaises(TypeError):
-            max_integer([10, (20, 30)])
+            max_integer(string)
+
+if __name__ == "__main__":
+    unittest.main()
