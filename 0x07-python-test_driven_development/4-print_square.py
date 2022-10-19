ea04b494d76ccac18fe067b404a74e6d6a50790d
Sophie77shalom <sophyjeptoo999@gmail.com>
2022-10-18 20:49:48 -0700

    a function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/ptyhon3
+"""Function that prints a square with a character #.
+
+"""
+
+
+
+
+
+def print_square(size):
+
+    """Method: print a square with #.
+
+    Args:
+
+    size(int): size of the square
+
+    Return:
+
+    No return
+
+    """
+
+
+
+    if not isinstance(size, int):
+
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+
+        raise ValueError("size must be >= 0")
+
+
+
+    for i in range(size):
+
+        print("#" * size)
