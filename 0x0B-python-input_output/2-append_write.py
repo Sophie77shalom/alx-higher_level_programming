f2ae103db17a299ec6e3e5e14673eb5d3e5877d8
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-10-25 19:46:53 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Module for append_write method.
+Module for to_json_to-string method.
 """
 
 
-def append_write(filename="", text=""):
-    """append_write appends string at the end of a text file.
-    Args:
-        filename (str): name of file.
-            text (str): text to be appended to the file.
-        Returns: number of characters written.
+import json
+
+
+def to_json_string(my_obj):
     """
-    with open(filename, "a", encoding='utf-8') as a_file:
-        return a_file.write(text)
+    Returns JSON representation of an object.
+    """
+    return (json.dumps(my_obj))
